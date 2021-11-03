2c6fab1316c1fb6a5630c1fd792735153ce8f9dd
Gero <gersonhoa@gmail.com>
2021-11-02 22:42:50 -0300

    100% lint 100%js

--- script.js
+++ script.js
@@ -1,51 +1,50 @@
-let insertText = document.getElementById('text-input');
-let paragraf = document.getElementById('meme-text');
-let btnI = document.getElementById('meme-insert');
-let board = document.getElementById('meme-image-container');
-let canvas = document.getElementById('meme-image');
-let btnFire = document.getElementById('fire');
-let btnWater = document.getElementById('water');
-let btnEarth = document.getElementById('earth');
-let meme1 = document.getElementById('meme-1');
-let meme2 = document.getElementById('meme-2');
-let meme3 = document.getElementById('meme-3');
-let meme4 = document.getElementById('meme-4');
+const insertText = document.getElementById('text-input');
+const paragraf = document.getElementById('meme-text');
+const btnI = document.getElementById('meme-insert');
+const board = document.getElementById('meme-image-container');
+const canvas = document.getElementById('meme-image');
+const btnFire = document.getElementById('fire');
+const btnWater = document.getElementById('water');
+const btnEarth = document.getElementById('earth');
+const meme1 = document.getElementById('meme-1');
+const meme2 = document.getElementById('meme-2');
+const meme3 = document.getElementById('meme-3');
+const meme4 = document.getElementById('meme-4');
 
-//criando atalhos ^
-
-insertText.addEventListener('keyup', changeText);
-btnI.addEventListener('click', bkgdSet);
+// Criando atalhos ^
 
 function changeText() {
   paragraf.innerText = insertText.value;
 }
 
-//add texto ao meme ^
+// Add texto ao meme ^
 
 function bkgdSet() {
   canvas.src = URL.createObjectURL(btnI.files[0]);
 }
-//Seta a imagem do meme ^
+// Seta a imagem do meme ^
 
 function bkgdSetII(meme) {
   console.log('MEMEME');
   canvas.src = meme.target.src;
 }
-//Seta a imagem do meme pré definido ^
+// Seta a imagem do meme pré definido ^
 
 function BordaFogo() {
   board.style.border = 'dashed red 3px';
 }
-//muda propriedades da borda ^
+// Muda propriedades da borda ^
 function BordaAgua() {
   board.style.border = 'double blue 5px';
 }
-//muda propriedades da borda ^
+// Muda propriedades da borda ^
 function BordaTerra() {
   board.style.border = 'groove green 6px';
 }
-//muda propriedades da borda ^
+// Muda propriedades da borda ^
 
+insertText.addEventListener('keyup', changeText);
+btnI.addEventListener('click', bkgdSet);
 btnI.addEventListener('change', bkgdSet);
 btnFire.addEventListener('click', BordaFogo);
 btnWater.addEventListener('click', BordaAgua);
@@ -55,4 +54,4 @@ meme1.addEventListener('click', bkgdSetII);
 meme2.addEventListener('click', bkgdSetII);
 meme3.addEventListener('click', bkgdSetII);
 meme4.addEventListener('click', bkgdSetII);
-//Ouvintes de evento
+// Ouvintes de evento
